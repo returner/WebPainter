a965e1192c7e9984ae2953027f33f78d4df6d2f2
returner <returner@daum.net>
2019-05-22 15:01:50 +0900

    knockoutjs및 jquery 추가

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,9 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { Album } from "./util/Album";
+//import fabric from "fabric/fabric-impl";
+import fabric from "fabric";
+import $ from "jquery";
 
 // let canvas = new Album();
 // canvas.CreateCanvas("canvas").add(new fabric.Rect({
@@ -12,5 +15,27 @@ import "bootstrap/dist/css/bootstrap.css";
 // }));
 export class Main
 {
+    public StartUp(mainContentCanvasId : string)
+    {
+        console.log("startup");
+        let canvas = new Album();
 
+        canvas.CreateCanvas(mainContentCanvasId).add(new fabric.fabric.Rect({
+            top : 10,
+            left : 10,
+            width : 10,
+            height : 10,
+            fill : 'blue'
+        }))
+    }
 }
+
+
+
+var editor = new Main();
+
+$("document").ready(()=>{
+    $("#btnCreate").click(()=>{
+        alert("click");
+    });
+});
